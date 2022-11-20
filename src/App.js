91e6e09b7185eea7adcefc1468c9664c021ed8f4
Adom <adomfosugit@gmail.com>
2022-11-20 16:01:10 +0000

    first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
-import Header from './components/Header'
-import Mainbody from './components/Mainbody';
-import Footer from './components/Footer'
+import styles from './style';
+import {Navbar,Hero,Stats,Business, Footer,CardDeal, CTA,Billing,Clients}  from './components'
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Mainbody />
-      <Footer />
-      
+    <div className="bg-primary w-full overflow-hidden">
+          <div className = { `${styles.paddingX} `}>
+            <div className = { `${styles.boxWidth}`}></div>
+              <Navbar />
+           </div>
+
+    <div className = {`bg-primary ${styles.flexStart}`}>
+      <div className = {`${styles.boxWidth}`}>
+      <Hero />
+
+      </div>
+          
+
+     </div>
+     
+    <div className = {`bg-primary ${styles.paddingX} ${styles.flexStart} `}>
+      <div className = {`${styles.boxWidth}`}></div>
+          <Stats />
+           <Business /> 
+          <Billing />
+           <CardDeal /> 
+          <Clients />
+           <CTA />
+           <Footer />
+
+ 
+    </div> 
       
     </div>
   );
